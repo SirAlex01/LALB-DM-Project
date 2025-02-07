62b31e4f905b0f2b8f2e84fdab8008b5518e7d7c
kev187038 <gabriele.matini18@gmail.com>
2025-02-07 13:22:39 +0100

    Transliterated homeric greek

--- transliter_iliad-odyssey.py
+++ transliter_iliad-odyssey.py
@@ -4,10 +4,12 @@ from greek_accentuation.syllabify import syllabify, display_word
 #from cltk.tokenize.sentence import TokenizeSentence
 from nltk.tokenize.punkt import PunktLanguageVars
 #from cltk.stop.greek.stops import STOPS_LIST
-from cltk.phonology.greek.transcription import Transcriber
+from cltk.phonology.grc.transcription import Transcriber
 import pandas as pd
+import unicodedata
 import spacy
 from IPython.display import display
+
 
 def transliteration_iliad_and_odyssey(path, path2):
 
@@ -26,41 +28,56 @@ def transliteration_iliad_and_odyssey(path, path2):
 	df2['OGTXT'] = df2.loc[:, 'greek text']
 	df2 = df2.drop('greek text', axis=1)
 	
-	dictionary = ()
+	dictionary = set()
 	
 	res = pd.DataFrame(columns=['word', 'transliteration'])
 	
-	#Lemmatize df1
+	#Transcribe df1 (ILIAD)
 	for index, row in df.iterrows():
 		original = row["TRANSLIT"]
 		
 		if isinstance(original, str) and not original.strip()=="":
 			doc = nlp(original)
 			words = [token.text for token in doc if not token.is_punct]
+
 			for word in words:
 				if  word not in dictionary:
 					dictionary.add(word)
+					#word = unicodedata.normalize("NFC", word) #need to remove diacritics (dieresis) otherwise it gives error
+					#word = elision_dict.get(word, word) #we need to remove contracted forms otherwise it again fails the transcription
 					transcribed_word = transcriber.transcribe(word)
+					print(word, transcribed_word)
 					res.loc[len(res)] = [word, transcribed_word]
-			display(res)
 
-	#Lemmatize df2
+	
+	#Transcribe df2 (ODYSSEY)
 	for index, row in df2.iterrows():
 		original = row["TRANSLIT"]
-
+		
 		if isinstance(original, str) and not original.strip()=="":
-			doc=nlp(original)
+			doc = nlp(original)
 			words = [token.text for token in doc if not token.is_punct]
+
 			for word in words:
 				if  word not in dictionary:
 					dictionary.add(word)
 					transcribed_word = transcriber.transcribe(word)
+					print(word, transcribed_word)
 					res.loc[len(res)] = [word, transcribed_word]
-
 
 	return res
 
-
+nlp = spacy.load("grc_odycy_joint_trf")
 path1 = './ILIADKEY.csv'
 path2 ='./gr_eng_text.csv'
 
+elision_dict = { #need to remove elisions to be processed correctly
+    "δ'": "δέ",
+    "θ'": "τε",
+    "γ'": "γε",
+    "ἀπ'": "ἀπό",
+    "καθ'": "κατά"
+}
+
+res = transliteration_iliad_and_odyssey(path1, path2)
+res.to_csv('./transliteration_homeric_greek. csv', index=False)
