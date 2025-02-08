61db65470f8d7a581719000ab2db599542991f78
SirAlex01 <113248340+SirAlex01@users.noreply.github.com>
2025-02-08 23:09:52 +0100

    before manual correction

--- LB-webscrape-documents.py
+++ LB-webscrape-documents.py
@@ -98,7 +98,7 @@ with open("signs_LB.csv", "w", encoding="utf-8",newline="") as csvfile:
                 while j < len(doc_text):
                     s = doc_text[j]
                     if s in "[],/↓→↗⸤⸥/⟦⟧:×" or s.startswith(".") or s.endswith(".") or s=="vacat" or s == "vest." or s == "deest" or s == "margo" or \
-                        s == "reliqua" or s == "pars" or s == "sine" or s == "regulis" or s == "prior" or s == "Graffito":
+                        s == "reliqua" or s == "pars" or s == "sine" or s == "regulis" or s == "prior" or s == "Graffito" or s == "vestigia":
                         j += 1
                         continue
 
@@ -126,7 +126,7 @@ with open("signs_LB.csv", "w", encoding="utf-8",newline="") as csvfile:
                         s = s[:-1]
 
                     if len(s) > 0 and not (s in "[],/↓→↗⸤⸥/⟦⟧:×" or s.startswith(".") or s.endswith(".") or s=="vacat" or s == "vest." or s == "deest" or s == "margo" or \
-                        s == "reliqua" or s == "pars" or s == "sine" or s == "regulis" or s == "prior" or s == "Graffito"):
+                        s == "reliqua" or s == "pars" or s == "sine" or s == "regulis" or s == "prior" or s == "Graffito" or s == "vestigia"):
                         sequences.append(s)
                     
                     j += 1
