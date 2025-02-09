d0944e010904f1e9acf2eea5c8001efe2cfc1a2a
SirAlex01 <113248340+SirAlex01@users.noreply.github.com>
2025-02-09 04:38:06 +0100

    Revert "fixes"
    This reverts commit cef31f420239536cf06658a638e19b8fe6758f1c.

--- LB-webscrape-documents.py
+++ LB-webscrape-documents.py
@@ -97,8 +97,8 @@ with open("signs_LB.csv", "w", encoding="utf-8",newline="") as csvfile:
                 j = 0
                 while j < len(doc_text):
                     s = doc_text[j]
-                    if s in "[],/↓→↗⸤⸥/⟦⟧:×" or s.startswith(".") or s.endswith(".")  or s.startswith("(") or s.endswith(")") or s=="vacat" or s == "vest." or s == "deest" or s == "margo" or \
-                        s == "reliqua" or s == "pars" or s == "sine" or s == "regulis" or s == "prior" or s == "Graffito" or s == "vestigia" or s.startswith("II"):
+                    if s in "[],/↓→↗⸤⸥/⟦⟧:×" or s.startswith(".") or s.endswith(".") or s=="vacat" or s == "vest." or s == "deest" or s == "margo" or \
+                        s == "reliqua" or s == "pars" or s == "sine" or s == "regulis" or s == "prior" or s == "Graffito" or s == "vestigia":
                         j += 1
                         continue
 
@@ -106,7 +106,6 @@ with open("signs_LB.csv", "w", encoding="utf-8",newline="") as csvfile:
                     s = s.replace("[•]", "(TEMP)").replace("[-•-]","(TEMPP)").replace("'","").replace('"',"").replace("-]", "-").replace("[-", "-").replace("]-", "-") \
                          .replace("-[", "-").replace("--", "-").replace("?", "").replace("(TEMP)","[?]").replace("(TEMPP)","-[?]-").replace("•-•","[?]-[?]") \
                          .replace("⟦","").replace("⟧","").replace("[[","[").replace("]]","]").replace("⸥","").replace("⸤","").replace("•","[?]").replace("/","").replace(",","")\
-                         .replace("TELA^x", "TELAx").replace("OVIS[f", "OVISf").replace("OVIS[m", "OVISm").replace("OVIS]f", "OVISf").replace("OVIS]m", "OVISm")
                     
                     # skip all rows referring to sigillum
                     if s == "supra" or s == "sigillum":
@@ -126,8 +125,8 @@ with open("signs_LB.csv", "w", encoding="utf-8",newline="") as csvfile:
                     if s.endswith("-"):
                         s = s[:-1]
 
-                    if len(s) > 0 and not (s in "[],/↓→↗⸤⸥/⟦⟧:×" or s.startswith(".")  or s.startswith("(") or s.endswith(")") or s=="vacat" or s == "vest." or s == "deest" or s == "margo" or \
-                        s == "reliqua" or s == "pars" or s == "sine" or s == "regulis" or s == "prior" or s == "Graffito" or s == "vestigia" or s.startswith("II")):
+                    if len(s) > 0 and not (s in "[],/↓→↗⸤⸥/⟦⟧:×" or s.startswith(".") or s.endswith(".") or s=="vacat" or s == "vest." or s == "deest" or s == "margo" or \
+                        s == "reliqua" or s == "pars" or s == "sine" or s == "regulis" or s == "prior" or s == "Graffito" or s == "vestigia"):
                         sequences.append(s)
                     
                     j += 1
